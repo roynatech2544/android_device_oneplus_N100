ef98dcd5080f75402c4cc5b7ecaaf003941b6bd7
maxwen <max.weninger@gmail.com>
2019-11-24 23:38:56 +0100

    N100: enable TARGET_ENFORCE_AB_OTA_PARTITION_LIST
    to prevent partition changes during ab updates
    
    Change-Id: Iab772f6752a16d37485d528749fe8fb50060e86b
    Signed-off-by: rany <ranygh@riseup.net>

--- device.mk
+++ device.mk
@@ -43,11 +43,10 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+# tell update_engine to not change dynamic partition table during updates
+# needed since our qti_dynamic_partitions does not include
+# vendor and odm and we also dont want to AB update them
+TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
 
 # Encryption: Use common FBE decryption script
 BOARD_USES_QCOM_FBE_DECRYPTION := true
