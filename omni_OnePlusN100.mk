4c3ac8e5fc558eb7d6f3b6dff036e4370ab25069
roynatech2544 <whiteshell2544@naver.com>
2022-07-09 05:10:31 +0000

    N100: Initial TWRP device tree
    Made with SebaUbuntu's TWRP device tree generator
    Arch: arm64
    Manufacturer: oneplus
    Model: Oneplus N100
    Script version: 2.3.0

---
+++ omni_OnePlusN100.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from OnePlusN100 device
+$(call inherit-product, device/oneplus/OnePlusN100/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := OnePlusN100
+PRODUCT_NAME := omni_OnePlusN100
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := Oneplus N100
+PRODUCT_MANUFACTURER := oneplus
