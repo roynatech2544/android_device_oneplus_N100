1cff72c66f73f4c194539038ec447ec034bd674c
roynatech2544 <whiteshell2544@naver.com>
2022-07-10 12:13:41 +0000

    billie2: Change to correct codename, billie2

--- device.mk
+++ device.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-LOCAL_PATH := device/oneplus/OnePlusN100
+LOCAL_PATH := device/oneplus/billie2
 
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
--- twrp_billie2.mk
+++ twrp_billie2.mk
@@ -10,15 +10,15 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-# Inherit from OnePlusN100 device
-$(call inherit-product, device/oneplus/OnePlusN100/device.mk)
+# Inherit from billie2 device
+$(call inherit-product, device/oneplus/billie2/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := OnePlusN100
-PRODUCT_NAME := twrp_OnePlusN100
+PRODUCT_DEVICE := billie2
+PRODUCT_NAME := twrp_billie2
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus N100
 PRODUCT_MANUFACTURER := OnePlus
